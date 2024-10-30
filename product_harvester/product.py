650d9838c0f8ed5cebd593d25be56155e3cc1d7b
Róbert Jačko <robert.jacko@bloomreach.com>
2024-10-30 19:45:03 +0100

    Remove descriptions from Product

--- product_harvester/product.py
+++ product_harvester/product.py
@@ -4,9 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class Product(BaseModel):
-    name: str = Field(description="The name of the product", strict=True, min_length=1)
-    qty: float = Field(description="Quantity of the product", strict=True, gt=0)
-    qty_unit: Literal["l", "ml", "kg", "g", "pcs"] = Field(
-        description="The unit of the quantity (l, ml, kg, g, pcs)"
-    )
-    price: float = Field(description="Price of the product", strict=True, gt=0)
+    name: str = Field(strict=True, min_length=1)
+    qty: float = Field(strict=True, gt=0)
+    qty_unit: Literal["l", "ml", "kg", "g", "pcs"] = Field()
+    price: float = Field(strict=True, gt=0)
