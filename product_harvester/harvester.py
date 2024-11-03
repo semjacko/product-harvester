c24f277ff7c8780ee1a55e7d722fda36a343abd9
semjacko <robertjacko.stu@gmail.com>
2024-11-03 22:38:37 +0100

    Add error handling to harvester

--- product_harvester/harvester.py
+++ product_harvester/harvester.py
@@ -1,21 +1,80 @@
-from product_harvester.processors import ImageProcessor, ProcessingError
+import logging
+from typing import Any
+
+from product_harvester.processors import ImageProcessor, ProcessingError, ProcessingResult
 from product_harvester.product import Product
-from product_harvester.retrievers import ImageRetriever
+from product_harvester.retrievers import ImageLinksRetriever
+
+
+class HarvestError(Exception):
+    def __init__(self, msg: str, extra: dict[str, Any] = None):
+        self.msg = msg
+        self.extra = extra
+
+    def __eq__(self, other):
+        return isinstance(other, HarvestError) and self.msg == other.msg and self.extra == other.extra
+
+
+class ErrorTracker:
+    def track_errors(self, errors: list[HarvestError]):
+        raise NotImplementedError()
+
+
+class ErrorLogger(ErrorTracker):
+    def track_errors(self, errors: list[HarvestError]):
+        for error in errors:
+            logging.error(msg=error.msg, extra=error.extra)
 
 
 class ProductsHarvester:
-    def __init__(self, retriever: ImageRetriever, processor: ImageProcessor):
+    def __init__(
+        self,
+        retriever: ImageLinksRetriever,
+        processor: ImageProcessor,
+        error_tracker: ErrorTracker = ErrorLogger(),
+    ):
         self._retriever = retriever
         self._processor = processor
+        self._error_tracker = error_tracker
 
     def harvest(self) -> list[Product]:
-        encoded_images = self._retriever.retrieve_images()
-        if not encoded_images:
+        image_links = self._retrieve_image_links()
+        result = self._process_images(image_links)
+        return self._extract_products_and_track_errors(result)
+
+    def _retrieve_image_links(self) -> list[str]:
+        try:
+            image_links = self._retriever.retrieve_image_links()
+        except Exception as e:
+            self._track_errors([HarvestError(str(e), {"info": "failed to retrieve image links"})])  # TODO: input
             return []
-        result = self._processor.process(encoded_images)
-        self._track_errors(result.errors)
+        return image_links
+
+    def _process_images(self, image_links: list[str]) -> ProcessingResult | None:
+        if not image_links:
+            return None
+        try:
+            result = self._processor.process(image_links)
+        except Exception as e:
+            self._track_errors(
+                [HarvestError(str(e), {"input": image_links, "info": "failed to extract data from all images"})]
+            )
+            return None
+        return result
+
+    def _extract_products_and_track_errors(self, result: ProcessingResult | None) -> list[Product]:
+        if not result:
+            return []
+        self._track_processing_errors(result.errors)
         return result.products
 
-    @staticmethod
-    def _track_errors(errors: list[ProcessingError]):
-        print(errors)  # TODO
+    def _track_processing_errors(self, processing_errors: list[ProcessingError]):
+        errors = [
+            HarvestError(error.error, {"input": error.input, "info": "failed to extract data from single image"})
+            for error in processing_errors
+        ]
+        self._track_errors(errors)
+
+    def _track_errors(self, errors: list[HarvestError]):
+        if errors:
+            self._error_tracker.track_errors(errors)
--- product_harvester/tests/test_harvester.py
+++ product_harvester/tests/test_harvester.py
@@ -1,20 +1,56 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import call, Mock, patch
 
-from product_harvester.harvester import ProductsHarvester
-from product_harvester.processors import ProcessingResult
+from product_harvester.harvester import ErrorLogger, ErrorTracker, HarvestError, ProductsHarvester
+from product_harvester.processors import ProcessingError, ProcessingResult
 from product_harvester.product import Product
+
+
+class TestErrorTracker(TestCase):
+    def test_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            ErrorTracker().track_errors([])
+
+
+class TestErrorLogger(TestCase):
+    @patch("product_harvester.harvester.logging.error")
+    def test_empty(self, logger_mock):
+        ErrorLogger().track_errors([])
+        logger_mock.assert_not_called()
+
+    @patch("product_harvester.harvester.logging.error")
+    def test_one_error(self, logger_mock):
+        ErrorLogger().track_errors([HarvestError("some message", {"info": "additional info"})])
+        logger_mock.assert_called_once_with(msg="some message", extra={"info": "additional info"})
+
+    @patch("product_harvester.harvester.logging.error")
+    def test_multiple_errors(self, logger_mock):
+        ErrorLogger().track_errors(
+            [
+                HarvestError("some message", {"info": "additional info"}),
+                HarvestError("error with no extra"),
+                HarvestError("", {"extra": "only extra"}),
+            ]
+        )
+        logger_mock.assert_has_calls(
+            [
+                call(msg="some message", extra={"info": "additional info"}),
+                call(msg="error with no extra", extra=None),
+                call(msg="", extra={"extra": "only extra"}),
+            ]
+        )
 
 
 class TestProductsHarvester(TestCase):
     def setUp(self):
         self._mock_retriever = Mock()
         self._mock_processor = Mock()
-        self._harvester = ProductsHarvester(self._mock_retriever, self._mock_processor)
+        self._mock_tracker = Mock()
+        self._harvester = ProductsHarvester(self._mock_retriever, self._mock_processor, self._mock_tracker)
 
     def test_harvest_returns_products(self):
-        mock_images = ["<encoded_image1>", "<encoded_image2>"]
-        self._mock_retriever.retrieve_images.return_value = mock_images
+        mock_image_links = ["/image1.jpg", "/image2.png"]
+        self._mock_retriever.retrieve_image_links.return_value = mock_image_links
         mock_products = [
             Product(name="Banana", qty=1.0, qty_unit="kg", price=1.99),
             Product(name="Milk", qty=500, qty_unit="ml", price=0.99),
@@ -23,15 +59,78 @@ class TestProductsHarvester(TestCase):
 
         products = self._harvester.harvest()
 
-        self._mock_retriever.retrieve_images.assert_called_once()
-        self._mock_processor.process.assert_called_once_with(mock_images)
+        self._mock_retriever.retrieve_image_links.assert_called_once()
+        self._mock_processor.process.assert_called_once_with(mock_image_links)
+        self._mock_tracker.track_errors.assert_not_called()
+        self.assertEqual(products, mock_products)
+
+    def test_harvest_returns_product_and_errors(self):
+        mock_image_links = ["/image1.jpg", "/wat.jpeg", "/wtf.png"]
+        self._mock_retriever.retrieve_image_links.return_value = mock_image_links
+        mock_products = [Product(name="Bread", qty=3, qty_unit="pcs", price=3.35)]
+        self._mock_processor.process.return_value = ProcessingResult(
+            mock_products,
+            [
+                ProcessingError({"link": "/wat.jpeg"}, "invalid image mocked error"),
+                ProcessingError({"link": "/wtf.png"}, "invalid JSON extracted mocked error"),
+            ],
+        )
+
+        products = self._harvester.harvest()
+
+        self._mock_retriever.retrieve_image_links.assert_called_once()
+        self._mock_processor.process.assert_called_once_with(mock_image_links)
+        self._mock_tracker.track_errors.assert_called_once_with(
+            [
+                HarvestError(
+                    "invalid image mocked error",
+                    {"input": {"link": "/wat.jpeg"}, "info": "failed to extract data from single image"},
+                ),
+                HarvestError(
+                    "invalid JSON extracted mocked error",
+                    {"input": {"link": "/wtf.png"}, "info": "failed to extract data from single image"},
+                ),
+            ]
+        )
         self.assertEqual(products, mock_products)
 
     def test_harvest_empty_retriever_result(self):
-        self._mock_retriever.retrieve_images.return_value = []
+        self._mock_retriever.retrieve_image_links.return_value = []
 
         result = self._harvester.harvest()
 
-        self._mock_retriever.retrieve_images.assert_called_once()
+        self._mock_retriever.retrieve_image_links.assert_called_once()
         self._mock_processor.process.assert_not_called()
+        self._mock_tracker.track_errors.assert_not_called()
+        self.assertEqual(result, [])
+
+    def test_harvest_retriever_error(self):
+        self._mock_retriever.retrieve_image_links.side_effect = ValueError("Something went wrong during retrieval")
+
+        result = self._harvester.harvest()
+
+        self._mock_retriever.retrieve_image_links.assert_called_once()
+        self._mock_processor.process.assert_not_called()
+        self._mock_tracker.track_errors.assert_called_once_with(
+            [HarvestError("Something went wrong during retrieval", {"info": "failed to retrieve image links"})]
+        )
+        self.assertEqual(result, [])
+
+    def test_harvest_processor_error(self):
+        mock_image_links = ["/image1.png", "/image2.jpeg"]
+        self._mock_retriever.retrieve_image_links.return_value = mock_image_links
+        self._mock_processor.process.side_effect = ValueError("Something went wrong during processing")
+
+        result = self._harvester.harvest()
+
+        self._mock_retriever.retrieve_image_links.assert_called_once()
+        self._mock_processor.process.assert_called_once_with(mock_image_links)
+        self._mock_tracker.track_errors.assert_called_once_with(
+            [
+                HarvestError(
+                    "Something went wrong during processing",
+                    {"input": ["/image1.png", "/image2.jpeg"], "info": "failed to extract data from all images"},
+                )
+            ]
+        )
         self.assertEqual(result, [])
