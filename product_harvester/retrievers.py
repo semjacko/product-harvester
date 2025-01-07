45165a849cd1fde16c5a8a613a3671a8997bb048
semjacko <robertjacko.stu@gmail.com>
2025-01-07 22:24:14 +0100

    Implement GoogleDriveImageLinksRetriever

--- product_harvester/retrievers.py
+++ product_harvester/retrievers.py
@@ -1,6 +1,14 @@
+import base64
+import io
 import os
 from glob import glob
-from typing import Generator
+from typing import Generator, Any, NamedTuple
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import Resource, build
+from googleapiclient.http import MediaIoBaseDownload
 
 
 class ImageLinksRetriever:
@@ -27,7 +35,86 @@ class LocalImageLinksRetriever(ImageLinksRetriever):
         return glob(path_pattern)
 
 
+class _GoogleDriveFileInfo(NamedTuple):
+    id: str
+    mime_type: str
+
+
+class _GoogleDriveClient:
+    _scopes = [
+        "https://www.googleapis.com/auth/drive.metadata.readonly",
+        "https://www.googleapis.com/auth/drive.readonly",
+    ]
+
+    def __init__(self, client_config: dict[str, Any]):
+        self._client_config = client_config
+        self._credentials: Credentials | None = None
+        self._files_service: Resource | None = None
+
+    def _ensure_credentials(self):
+        if self._has_valid_credentials():
+            return
+        elif self._can_refresh_credentials():
+            self._credentials.refresh(Request())
+        else:
+            self._load_credentials_from_consent_screen()
+        self._files_service = build("drive", "v3", credentials=self._credentials).files()
+
+    def _has_valid_credentials(self) -> bool:
+        return self._credentials and self._credentials.valid
+
+    def _can_refresh_credentials(self) -> bool:
+        return self._credentials and self._credentials.expired and self._credentials.refresh_token
+
+    def _load_credentials_from_consent_screen(self):
+        flow = InstalledAppFlow.from_client_config(self._client_config, self._scopes)
+        self._credentials = flow.run_local_server(port=0)
+
+    def get_image_files_info(self, folder_id: str) -> Generator[_GoogleDriveFileInfo, None, None]:
+        query = f"'{folder_id}' in parents and mimeType contains 'image/'"
+        files, page_offset_token = self._get_image_files_batch(query)
+        while page_offset_token:
+            yield from files
+            files, page_offset_token = self._get_image_files_batch(query, page_offset_token)
+        yield from files
+
+    def _get_image_files_batch(
+        self,
+        query: str,
+        page_offset_token: str | None = None,
+        batch_size: int = 8,
+    ) -> tuple[list[_GoogleDriveFileInfo], str | None]:
+        self._ensure_credentials()
+        request = self._files_service.list(
+            pageSize=batch_size, q=query, fields="nextPageToken, files(id, mimeType)", pageToken=page_offset_token
+        )
+        result = request.execute()
+        files = [
+            _GoogleDriveFileInfo(id=file.get("id", ""), mime_type=file.get("mimeType", "")) for file in result["files"]
+        ]
+        return files, result.get("nextPageToken", None)
+
+    def download_file_content(self, file: _GoogleDriveFileInfo) -> str:
+        self._ensure_credentials()
+        request = self._files_service.get_media(fileId=file.id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+        content = base64.b64encode(fh.getvalue()).decode("utf-8")
+        return f"data:{file.mime_type};base64,{content}"
+
+
 class GoogleDriveImageLinksRetriever(ImageLinksRetriever):
+
+    def __init__(self, client_config: dict[str, Any], folder_id: str):
+        self._client = _GoogleDriveClient(client_config)
+        self._folder_id = folder_id
+
+    def set_folder(self, folder_id: str):
+        self._folder_id = folder_id
+
     def retrieve_image_links(self) -> Generator[str, None, None]:
-        # TODO
-        raise NotImplementedError()
+        for file in self._client.get_image_files_info(self._folder_id):
+            yield self._client.download_file_content(file)
--- product_harvester/tests/test_retrievers.py
+++ product_harvester/tests/test_retrievers.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from product_harvester.retrievers import (
-    GoogleDriveImageLinksRetriever,
     ImageLinksRetriever,
     LocalImageLinksRetriever,
+    GoogleDriveImageLinksRetriever,
+    _GoogleDriveFileInfo,
 )
 
 
@@ -68,6 +69,53 @@ class TestLocalImageLinksRetriever(TestCase):
 
 
 class TestGoogleDriveImageLinksRetriever(TestCase):
-    def test_not_implemented(self):
-        with self.assertRaises(NotImplementedError):
-            GoogleDriveImageLinksRetriever().retrieve_image_links()
+    def setUp(self):
+        self._test_client_config = {
+            "installed": {
+                "client_id": "client_abc",
+                "project_id": "some",
+                "client_secret": "very-secret",
+                "redirect_uris": ["http://localhost"],
+            }
+        }
+        self._test_folder_id = "abc"
+
+    @patch("product_harvester.retrievers._GoogleDriveClient")
+    def test_simple(self, mocked_client):
+        mock_client = mocked_client.return_value
+        test_files = [
+            _GoogleDriveFileInfo(id="file_id_1", mime_type="image/png"),
+            _GoogleDriveFileInfo(id="file_id_2", mime_type="image/jpeg"),
+        ]
+        mock_client.get_image_files_info.return_value = iter(test_files)
+        test_contents = ["/some/binary", "/another/binary"]
+        mock_client.download_file_content.side_effect = test_contents
+        retriever = GoogleDriveImageLinksRetriever(self._test_client_config, self._test_folder_id)
+        self.assertEqual(list(retriever.retrieve_image_links()), test_contents)
+        mock_client.get_image_files_info.assert_called_once_with(self._test_folder_id)
+        mock_client.download_file_content.assert_has_calls([call(test_file) for test_file in test_files])
+
+    @patch("product_harvester.retrievers._GoogleDriveClient")
+    def test_change_folder(self, mocked_client):
+        mock_client = mocked_client.return_value
+        test_file = _GoogleDriveFileInfo(id="file_id_1", mime_type="image/png")
+        mock_client.get_image_files_info.return_value = iter([test_file])
+        test_contents = ["/some/binary"]
+        mock_client.download_file_content.side_effect = test_contents
+        retriever = GoogleDriveImageLinksRetriever(self._test_client_config, self._test_folder_id)
+        retriever.set_folder("other_folder")
+        self.assertEqual(list(retriever.retrieve_image_links()), test_contents)
+        mock_client.get_image_files_info.assert_called_once_with("other_folder")
+        mock_client.download_file_content.assert_called_once_with(test_file)
+
+    @patch("product_harvester.retrievers._GoogleDriveClient")
+    def test_failure(self, mocked_client):
+        mock_client = mocked_client.return_value
+        test_file = _GoogleDriveFileInfo(id="file_id_1", mime_type="image/png")
+        mock_client.get_image_files_info.return_value = iter([test_file])
+        mock_client.download_file_content.side_effect = ValueError("Some error")
+        retriever = GoogleDriveImageLinksRetriever(self._test_client_config, self._test_folder_id)
+        with self.assertRaisesRegex(ValueError, "Some error"):
+            list(retriever.retrieve_image_links())
+        mock_client.get_image_files_info.assert_called_once_with(self._test_folder_id)
+        mock_client.download_file_content.assert_called_once_with(test_file)
