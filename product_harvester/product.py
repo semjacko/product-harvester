1ea48df9a63b716d05e91cbcae4b0a2d1d1ccbd0
semjacko <robertjacko.stu@gmail.com>
2025-02-14 19:25:24 +0100

    Improve Product model

--- product_harvester/product.py
+++ product_harvester/product.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseModel, Field
 
@@ -8,6 +8,6 @@ class Product(BaseModel):
     qty: float = Field(strict=True, gt=0)
     qty_unit: Literal["l", "ml", "kg", "g", "pcs"] = Field()
     price: float = Field(strict=True, gt=0)
-    barcode: int = Field(strict=True, gt=-1, default=0)
-    brand: str = Field(strict=True, default="")
+    barcode: Optional[int] = Field(strict=True, gt=-1, default=0)
+    brand: Optional[str] = Field(strict=True, default="")
     category: str = Field(strict=True, min_length=1)
--- product_harvester/tests/test_product.py
+++ product_harvester/tests/test_product.py
@@ -7,10 +7,22 @@ from product_harvester.product import Product
 
 class TestProduct(TestCase):
     def test_ok(self):
-        Product(name="Banana", qty=1.5, qty_unit="kg", price=1, barcode=123, brand="", category="fruit")
-        Product(name="Banana", qty=1.5, qty_unit="kg", price=1, barcode=123, category="fruit")
-        Product(name="Milk", qty=1000, qty_unit="ml", price=10, barcode=456, brand="Rajo", category="milk")
-        Product(name="Kiwi", qty=3, qty_unit="pcs", price=10.54, barcode=567, category="fruit")
+        # language=JSON
+        Product.model_validate_json(
+            '{"name":"Banana", "qty":1.5, "qty_unit":"kg", "price":1, "barcode":123, "brand":null, "category":"fruit"}'
+        )
+        # language=JSON
+        Product.model_validate_json(
+            '{"name":"Banana", "qty":1.5, "qty_unit":"kg", "price":1, "barcode":null, "brand":"", "category":"fruit"}'
+        )
+        # language=JSON
+        Product.model_validate_json(
+            '{"name":"Milk", "qty":1000, "qty_unit":"ml", "price":10, "brand":"Rajo", "category":"milk"}'
+        )
+        # language=JSON
+        Product.model_validate_json(
+            '{"name":"Kiwi", "qty":3, "qty_unit":"pcs", "price":10.54, "barcode":0, "category":"fruit"}'
+        )
 
     def test_empty(self):
         with self.assertRaises(ValidationError):
@@ -18,30 +30,57 @@ class TestProduct(TestCase):
 
     def test_empty_name(self):
         with self.assertRaises(ValidationError):
-            Product(name="", qty=10, qty_unit="kg", price=10, barcode=123, category="something")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"", "qty":10, "qty_unit":"kg", "price":10, "barcode":123, "category":"something"}'
+            )
 
     def test_invalid_qty(self):
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=0, qty_unit="kg", price=10, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":0, "qty_unit":"kg", "price":10, "barcode":123, "category":"fruit"}'
+            )
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=-2, qty_unit="kg", price=10, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":-2, "qty_unit":"kg", "price":10, "barcode":123, "category":"fruit"}'
+            )
 
     def test_invalid_qty_unit(self):
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="", price=10, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"", "price":10, "barcode":123, "category":"fruit"}'
+            )
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="wat", price=10, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"wat", "price":10, "barcode":123, "category":"fruit"}'
+            )
 
     def test_invalid_price(self):
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="kg", price=0, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"kg", "price":0, "barcode":123, "category":"fruit"}'
+            )
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="kg", price=-2, barcode=123, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"kg", "price":-2, "barcode":123, "category":"fruit"}'
+            )
 
     def test_invalid_barcode(self):
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="kg", price=10, barcode=-1, category="fruit")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"kg", "price":10, "barcode":-1, "category":"fruit"}'
+            )
 
     def test_empty_category(self):
         with self.assertRaises(ValidationError):
-            Product(name="Banana", qty=10, qty_unit="kg", price=10, barcode=123, category="")
+            # language=JSON
+            Product.model_validate_json(
+                '{"name":"Banana", "qty":10, "qty_unit":"kg", "price":10, "barcode":123, "category":""}'
+            )
